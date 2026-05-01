c72c8644215934ce42d02b38d310ca497729883a
David <davidcaetanoribeiro@gmail.com>
2026-05-01 18:20:11 -0300

    build: build inicial do projeto

---
+++ src/main/java/dev/davidcaetanor/daily_leetcode_java/DailyLeetcodeJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.davidcaetanor.daily_leetcode_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyLeetcodeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyLeetcodeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/davidcaetanor/daily_leetcode_java/DailyLeetcodeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davidcaetanor.daily_leetcode_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyLeetcodeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
